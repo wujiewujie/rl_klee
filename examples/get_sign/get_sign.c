9305c3e5e2dbc7e3eae717d174444fb71ee7d5ec
wujie <15000967816@163.com>
2019-04-09 12:39:57 +0000

    interact with python to get to one target point

--- examples/get_sign/get_sign.c
+++ examples/get_sign/get_sign.c
@@ -4,18 +4,27 @@
 
 #include <klee/klee.h>
 
+
 int get_sign(int x) {
-  if (x == 0)
-     return 0;
-  
-  if (x < 0)
-     return -1;
-  else 
-     return 1;
-} 
+    if(x>0){
+        if(x<1){
+            klee_stop();
+            return 0;
+        }else{
+            return 1;
+        }
+    }else{
+        if(x<-1){
+            return 2;
+        }else{
+            return 3;
+        }
+    }
+}
 
 int main() {
-  int a;
-  klee_make_symbolic(&a, sizeof(a), "a");
-  return get_sign(a);
+    klee_connect();
+    int a;
+    klee_make_symbolic(&a, sizeof(a), "a");
+    return get_sign(a);
 } 
---
+++ main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by lab301 on 4/1/19.
+//
+#include <stdio.h>
+int main(){
+    printf("hhh\n");   
+}
